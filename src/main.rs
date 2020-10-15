7ff8215e95c627c3e18e91587711a783d2977233
Ubuntu <ubuntu@ip-172-31-9-101.us-west-1.compute.internal>
2020-10-15 20:54:14 +0000

    idk

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 pub mod source_file;
 
-use std::io;
-use std::io::{BufReader, BufRead}; //BufRead implements the lines trait
-use std::env;
-use std::path::{Path, PathBuf};
-use std::fs::{self, DirEntry, File};
 use std::collections::HashMap;
-use regex::Regex;
+use std::env;
+use std::fs::{self, DirEntry};
+use std::io;
+use std::io::{BufRead, BufReader}; //BufRead implements the lines trait
+use std::path::Path;
 
-use source_file::{SourceFiles, SourceFile, FuncSignature};
+use source_file::{FuncSignature, SourceFile, SourceFiles};
 
 const function_pats: &'static [&'static str] = &[r"(public|private).+{$"];
 
@@ -20,7 +19,7 @@ fn main() {
 
     // need to declare a mutable reference to the struct first
     let mut files = SourceFiles::new();
-    
+
     // iterate directory
     visit_dirs(path, &path_cb, &mut files);
 
@@ -34,33 +33,35 @@ fn path_cb(entry: &DirEntry, mut files: &mut SourceFiles) -> () {
     let path = entry.path();
     // this doesnt work: figure out why?
     //let str_path = path.to_str()?;
-    let str_path = path.to_str().unwrap(); 
+    let str_path = path.to_str().unwrap();
 
     if str_path.contains(".java") {
-
         let mut source_file = SourceFile {
-            file_path : path,
-            func_list : Vec::new()
+            file_path: path,
+            func_list: HashMap::<String, FuncSignature>::new(),
         };
 
         files.files.push(source_file);
-        
+
         //println!("{}", str_path);
-    }   
+    }
 }
 
-fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry, &mut SourceFiles), mut files: &mut SourceFiles) -> io::Result<()>{
+fn visit_dirs(
+    dir: &Path,
+    cb: &dyn Fn(&DirEntry, &mut SourceFiles),
+    mut files: &mut SourceFiles,
+) -> io::Result<()> {
     if dir.is_dir() {
-        for entry in fs::read_dir(dir)?{
+        for entry in fs::read_dir(dir)? {
             // Because entry is a Result object; most things in Rust are
             let entry = entry?;
             let path = entry.path();
-            
-            
+
             // if path is directory recurse
             if path.is_dir() {
                 // dont need to modify dir.path or dir so dont need mutable references
-                // also at this point entry lifetime 
+                // also at this point entry lifetime
                 visit_dirs(&path, &path_cb, &mut files);
             }
             // if file process with cb function
--- src/source_file.rs
+++ src/source_file.rs
@@ -1,10 +1,8 @@
 use regex::Regex;
 use std::collections::HashMap;
-use std::io::{BufReader, BufRead}; //BufRead implements the lines trait
+use std::fs::File;
+use std::io::{BufRead, BufReader}; //BufRead implements the lines trait
 use std::path::PathBuf;
-use std::fs:: File;
-use std::collections::HashMap;
-
 
 pub struct SourceFiles {
     pub files: Vec<SourceFile>,
@@ -12,12 +10,9 @@ pub struct SourceFiles {
 
 impl SourceFiles {
     pub fn new() -> SourceFiles {
-        SourceFiles {
-            files: Vec::new()
-        }
+        SourceFiles { files: Vec::new() }
     }
 }
-
 
 // COMMENT: System Design
 // In general it is wise to hide as much implementation detail as possible, and to only expose
@@ -25,23 +20,23 @@ impl SourceFiles {
 // allows people to think more easily about the system, since more irrelevant detail is hidden about
 // system interactions
 pub struct SourceFile {
-    // maps functions to the class where they are declared
-    // dont think I need separate class object since all we need is the name
-   // func_list: HashMap<String, FuncSignature>,
+    // FuncSignature mapped to class names
     pub func_list: HashMap<String, FuncSignature>,
     pub file_path: PathBuf,
 }
 
+// maybe rename
 pub struct FuncSignature {
     //TODO: add arguments and return type
-    name: String,
+    // name: String,
+    content: Vec<String>,
 }
 
 impl SourceFile {
     // the lifetime parameters say that the output variable's scope is tied to the scope of line
     // this assertion is checked by the Rust compiler during compile to make sure no dangling
     // pointers are being used
-    fn find_pat<'a>(line:&'a str, pat:&str) -> Option<&'a str> {
+    fn find_pat<'a>(line: &'a str, pat: &str) -> Option<&'a str> {
         let pattern = Regex::new(pat).unwrap();
         // is there no better way to handle this?
         // Get someone's opinion on this
@@ -49,68 +44,101 @@ impl SourceFile {
         match arg {
             None => None,
             Some(x) => {
-                let matched_arg = x.get(1)
+                let matched_arg = x
+                    .get(1)
                     .expect("Error getting first matching group")
                     .as_str();
-                return Some(matched_arg)
+                return Some(matched_arg);
             }
         }
     }
 
-    // 
-    fn parse_brackets(line: String, stack: Vec<u8>) {
-        for c in line.chrs() {
+    // parse brackets in a line and modify the function scope stack
+    // Cuurently assumes that brackets will be balanced ie. no declaration of "{" as a string
+    // literal or something
+    fn parse_brackets(line: &String, mut stack: &mut Vec<char>) -> bool {
+        for c in line.chars() {
+            match c {
+                // stack could only hold { since anything else would have been popped off
+                c if String::from(c) == "}" => {
+                    stack.pop();
+                    // empty stack means that the current scope have been closed
+                    if stack.is_empty() {
+                        return true;
+                    }
+                }
+                c if String::from(c) == "{" => {
+                    stack.push(c);
+                }
+                _ => (),
+            }
+            }
+            false
+        }
 
-        } 
-    }
-    
-    // parse functions name and args
-    // parse parents class structure
-    pub fn parse_source(&mut self) -> () {
-        //TODO: change this to use ? and modify function signature to return error
-        let input = File::open(&self.file_path).unwrap();
-        let buffered = BufReader::new(input);
-        println!("filepath: {}", self.file_path.to_str().unwrap());
-       
-        //OPTIMIZATION: allocate storage so that we dont need to reinitialize them each time
-        //parse_source is called
-        // Current function or class scope
-        let class = String::from("");
-        let function = String::from("");
-        
-        // Use these to determine the scoping levels
-        // holds pairs of brackets
-        // OPTIMIZATION: implement a push/pop that will automatically update some parameter holding
-        // the current function scope
-        let function_scope = Vec::new(u8);
-        let class_scope = Vec::new(u8);
+        // parse functions name and args
+        // parse parents class structure
+        pub fn parse_source(&mut self) -> () {
+            //TODO: change this to use ? and modify function signature to return error
+            let input = File::open(&self.file_path).unwrap();
+            let buffered = BufReader::new(input);
+            //println!("filepath: {}", self.file_path.to_str().unwrap());
 
+            //OPTIMIZATION: allocate storage so that we dont need to reinitialize them each time
+            //parse_source is called
+            // Current function or class scope
+            let class = String::from("");
+            let function = String::from("");
 
-        // Holds the lines to the buffer
-        let function_buf = Vec::<&String>;
-        
-        // Parse loop
-        // Naive parsing implementation, we make assumptions:
-        // 1.Look for function definitions (function signature followed by "}")
-        // 2.Assume no inner function definitions
-        // 3.Look for function calls
-        for line in buffered.lines() {
-            let line = line.unwrap();
-            if line.contains("class") {
-                println!("CLASS DETECTED: {}", line);
+            // Use these to determine the scoping levels
+            // holds pairs of brackets
+            // OPTIMIZATION: implement a push/pop that will automatically update some parameter holding
+            // the current function scope
+            let mut function_scope = Vec::<char>::new();
+            let mut class_scope = Vec::<char>::new();
+
+            // Holds the lines to the buffer
+            let function_buf = Vec::<&String>::new();
+
+            // Parse loop
+            // Naive parsing implementation, we make assumptions:
+            // 1.Look for function definitions (function signature followed by "}")
+            // 2.Assume no inner function definitions
+            // 3.Look for function calls
+            for line in buffered.lines() {
+                let line = line.unwrap();
+
+                // get function names and arguments
+                if line.contains("public") || line.contains("private") {
+                    if line.contains("class") {
+                        //println!("CLASS DETECTED: {}", line);
+                        println!("class: {}", line.split(" ").nth(2).unwrap_or("problem"));
+                    } else if line.contains("(") && line.contains(")") && line.trim_start().trim_end().
+                        let mut line_trimmed = line.split_whitespace();
+                    println!("        function name: {}", line);
+
+                    let func_arg = line_trimmed.nth(1).unwrap();
+                    let func_name = line_trimmed.nth(2).unwrap();
+
+                    //println!("SPLIT ARGS: {} {}", func_arg, func_name);
+                    for l in line_trimmed {
+                        println!("{}", l);
+                    }
+                }
             }
             // get function names and arguments
-            if line.contains("public") || line.contains("private") {
-                println!("{}", line);
-            }
-            
-            // parse brackets contained in the line and update our scopes
-            ParseSource::parse_brackets(line, function_scope);
-            ParseSource::parse_brackets(line, class_scope);
-                
-            // check if scope is closed
-            if function_scope
+            if line.contains("public") || line.contains("private") {}
 
-        } 
-    } 
+            // parse brackets contained in the line and update our scopes
+            let function_closed = SourceFile::parse_brackets(&line, &mut function_scope);
+            let class_closeed = SourceFile::parse_brackets(&line, &mut class_scope);
+
+            // if function scope is closed we have finished parsing a function
+            if function_closed {
+                for line in &function_buf {
+                    println!("{}", line);
+                }
+            }
+        }
+    }
 }
